60dfff6a59126075aea88e409374975fe30f8e47
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-01-13 16:45:53 +0000

    feat: created and attached js, css and html updated to introduce animated search bar

---
+++ static/js/script.js
@@ -0,0 +1,12 @@
+// search bar JS
+/*document.addEventListener("DOMContentLoaded", () => {
+  const searchBtn = document.getElementById("search-icon");
+  const searchBox = document.getElementById("search-box");
+
+  searchBtn.addEventListener("click", () => {
+    searchBox.classList.toggle("expanded");
+  });
+}); */
+
+console.log("JavaScript file connected successfully!");
+alert("JavaScript file connected successfully!");
