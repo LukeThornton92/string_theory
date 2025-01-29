15121c67c1205763a9e14af7d0e5911e2ac253d7
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-01-29 21:18:53 +0000

    chore: removed useless categories, modified the view to search for brand and category

--- products/views.py
+++ products/views.py
@@ -14,10 +14,16 @@ def all_products(request):
     
 
     if request.GET:
+        
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
-            products = products.filter(category__name__in=categories)
-            categories = Category.objects.filter(name__in=categories)
+            brand = request.GET.get('brand')
+            if categories:
+                products = products.filter(category__name__in=categories)
+                categories = Category.objects.filter(name__in=categories)
+            if brand:
+                products = products.filter(brand__iexact=brand)
+
 
         if 'q' in request.GET:
             query = request.GET['q']
