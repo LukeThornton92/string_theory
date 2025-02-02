b23a7c54c10155adef8e7e51b5bbfed82a460da5
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-02-02 18:18:49 +0000

    feat: added shopping bag app, urls and template

--- home/views.py
+++ home/views.py
@@ -5,11 +5,3 @@ from django.shortcuts import render
 def index(request):
     """A view to return the index page"""
     return render(request, 'home/index.html')
-
-#Django logic below for looping navbar, will work on later
-"""
-def your_view(request):
-    dropdown_titles = ['Title 1', 'Title 2', 'Title 3', 'Title 4', 
-                       'Title 5', 'Title 6', 'Title 7', 'Title 8']
-    return render(request, 'your_template.html', {'dropdown_titles': dropdown_titles})
-    """
