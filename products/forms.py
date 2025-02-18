c21a6cf0354e1f6bda51c68c4db46caa7c687481
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-02-18 18:43:01 +0000

    feat: fixing the image field with widget

--- products/forms.py
+++ products/forms.py
@@ -1,6 +1,6 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
-
 
 
 class ProductForm(forms.ModelForm):
@@ -9,6 +9,8 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
@@ -16,4 +18,4 @@ class ProductForm(forms.ModelForm):
 
         self.fields['category'].choices = friendly_names
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black round-0'
+            field.widget.attrs['class'] = 'border-black rounded-0'
