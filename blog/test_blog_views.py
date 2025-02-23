aed4985e98342dd855d8b48dffa66df6045cf544
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-02-23 17:22:48 +0000

    feat: header links updated and test updated

--- blog/test_blog_views.py
+++ blog/test_blog_views.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import BlogPost, Tag, BlogImage
+from datetime import datetime
 
 class BlogViewsTest(TestCase):
+    """ Test to see if Blog renders page"""
     def test_blog_view(self):
         url = reverse('blog')
         response = self.client.get(url)
