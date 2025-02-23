ccc047dae4d29c3b4f655f20e20eac48fd5d99e5
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-02-23 18:19:18 +0000

    fix: test were wrong, fixed, 2 fails. add-blog added

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.blog, name='blog'),
+    path('', views.all_blog_posts, name='blog'),
+    path('add/', views.add_blog_post, name='add_blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def blog(request):
+def all_blog_posts(request):
     """A view to return the blog page"""
     return render(request, 'blog/blog.html')
+
+def add_blog_post(request):
+    """A view to return the blog page"""
+    return render(request, 'blog/add-blog.html')
