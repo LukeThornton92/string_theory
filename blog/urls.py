e7f00782250dada6426aa8851d0ae2fac616cdf2
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-02-24 17:00:36 +0000

    feat: bootstrap pagination added and working

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_blog_posts, name='blog'),
-    path('<int:id>/', views.blog_detail, name='view_blog'),
+    path('<int:id>/', views.blog_detail, name='blog_detail'),
     path('add/', views.add_blog_post, name='add_blog'),
 ]
