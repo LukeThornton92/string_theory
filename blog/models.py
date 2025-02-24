af9d2a9e56c8f8dfce103fa0852020a5203c7031
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-02-24 18:16:41 +0000

    feat: model updated for Tags to be searchable, for loop added to blog.html

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,21 @@
-# blog/models.py
 from django.db import models
+from django.utils.text import slugify
+
+class Tag(models.Model):
+    """ Represents a tag for categorizing blog posts. """
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
 
 class Author(models.Model):
-    ''' Represents an author of a blog post. '''
+    """ Represents an author of a blog post. """
     name = models.CharField(max_length=100)
     bio = models.TextField(blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -11,18 +24,18 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
-    
+
 class BlogPost(models.Model):
-    ''' Represents a blog post. '''
+    """ Represents a blog post. """
     title = models.CharField(max_length=200, blank=False)
     content = models.TextField(blank=False)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    tags = models.CharField(max_length=100, blank=False)
+    tags = models.ManyToManyField(Tag, related_name="blog_posts")
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
-        ''' String representation of a BlogPost instance. '''
+        """ String representation of a BlogPost instance. """
         return self.title
