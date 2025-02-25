55bb2af3bedf418d70138c87fc5134208e32a555
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-02-25 23:16:07 +0000

    feat: about us and contact us made, with urls and views added to render, alot of small changes such as links

--- home/views.py
+++ home/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 def index(request):
     """A view to return the index page"""
     return render(request, 'home/index.html')
+
+def contact_us(request):
+    return render(request, 'home/contact_us.html')
+
+def about_us(request):
+    return render(request, 'home/about_us.html')
