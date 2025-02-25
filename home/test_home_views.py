4924365f571064a97a78e2010150ddfe1c79ccca
Luke Thornton <725682@student.cityofbristol.ac.uk>
2025-02-25 23:21:15 +0000

    feat: test views for about and contact

--- home/test_home_views.py
+++ home/test_home_views.py
@@ -7,3 +7,17 @@ class IndexViewTest(TestCase):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home/index.html')
+
+class ContactUsViewTest(TestCase):
+    def test_home_view(self):
+        """Test that the contact_us view returns the correct template and status code"""
+        response = self.client.get(reverse('contact_us'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/contact_us.html')
+
+class AboutUsViewTest(TestCase):
+    def test_home_view(self):
+        """Test that the about_us view returns the correct template and status code"""
+        response = self.client.get(reverse('about_us'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home/about_us.html')
